58e2305b11a3b135a8b0d65540d611fa3beb30cb
Vitaly-Baidin <vitaliy.baidin.a@gmail.com>
2022-07-29 12:27:29 +0300

    init

---
+++ src/main/java/com/viskei/flags/FlagsAppApplication.java
@@ -0,0 +1,13 @@
+package com.viskei.flags;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlagsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlagsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/viskei/flags/FlagsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viskei.flags;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlagsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
